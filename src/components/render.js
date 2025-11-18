8605930562b5959ffa21fc352dd7cf5d045c60ad
TheRookiexe <mishrxa36@gmail.com>
2025-11-18 23:21:40 +0530

    added weather icons

--- src/components/render.js
+++ src/components/render.js
@@ -7,12 +7,42 @@ class Renderer {
         this.container.appendChild(this.renderDiv);
     }
     
-    renderWeather(data) {
+    async renderWeather(data) {
         const currWeatherCard = document.createElement('div');
-        currWeatherCard.classList.add('curr-weather-card');
-        currWeatherCard.textContent = `${data.resolvedAddress}<br> Current Temp: ${Math.round((data.currentConditions.temp-32)*5/9)} °C`;
+        currWeatherCard.classList.add('curr-weather-card'); 
+        
+        const weatherIcon = document.createElement('img');
+        weatherIcon.id = "weather-icon";
+
+        const conditions = data.currentConditions.icon;
+        const iconUrl = await this.loadWeatherIcon(conditions);
+        weatherIcon.src = iconUrl;
+        
+        currWeatherCard.appendChild(weatherIcon);
+
+        const info = document.createElement('div');
+        info.classList.add('weather-info');
+        info.innerHTML = `
+        <center> <h2>${data.resolvedAddress}</h2> </center> <br>
+        Current Temp: ${Math.round((data.currentConditions.temp-32)*5/9)} °C <br>
+        Conditions: ${data.currentConditions.conditions} <br>
+        Wind Speed: ${data.currentConditions.windspeed} nots
+        `;
+
+        currWeatherCard.appendChild(info);
         this.renderDiv.appendChild(currWeatherCard);
     }
+
+    async loadWeatherIcon(condition) {
+    try {
+        const iconModule = await import(`../assets/${condition}.png`);
+        return iconModule.default; 
+    } catch (err) {
+        console.error("Icon not found:", condition);
+        const fallback = await import('../assets/default.png');
+        return fallback.default;
+    }
+}
 }
 
 export {Renderer};
